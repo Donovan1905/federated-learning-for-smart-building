3aed970372d6598b88437e4e4fd90bbaaf481681
Donovan <donovan.hoang@viacesi.fr>
2022-05-20 00:38:22 +0200

    fix loading, and load all csv into one dict

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+from json import load
+import os
+from pathlib import Path
+
 from wrangling.wrangling import load_data
 
-load_data(83)
+departments = [62, 83, 85, 91]
+datasets = dict()
+
+for department in departments:
+    df = load_data(department)
+    datasets[str(department)] = df
+
+for department in datasets:
+    print(datasets[department].head())
--- wrangling/wrangling.py
+++ wrangling/wrangling.py
@@ -3,10 +3,8 @@ import os
 import pandas as pd
 import numpy as np
 
-# pd.set_option('display.width', 40)
-
 def load_data(department):
     csv_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../_data/csv/', str(department) + '/batiment.csv')
-    print(csv_path)
-    csv = pd.read_csv(csv_path, sep=';')
-    print(csv.head())
+    print("loading... ", csv_path)
+    csv = pd.read_csv(csv_path, sep=';', low_memory=False)
+    return csv

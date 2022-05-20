3d0fff4da8804800d344755a732f7407b7d7cf34
Donovan <donovan.hoang@viacesi.fr>
2022-05-20 11:58:03 +0200

    analyze values, total line, NaN values, removing NaN values

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from json import load
 import os
 from pathlib import Path
 
-from wrangling.wrangling import load_data, analyze
+from wrangling.wrangling import load_data, analyze, drop_nan
 
 departments = [62, 83, 85, 91]
 datasets = dict()
@@ -12,7 +12,12 @@ for department in departments:
     df = load_data(department)
     datasets[str(department)] = df
 
-for department in datasets:
-    print(datasets[department].head())
+# for department in datasets:
+#     print(datasets[department].head())
 
 analyze(datasets)
+
+_data = drop_nan(datasets)
+
+analyze(_data)
+
--- wrangling/wrangling.py
+++ wrangling/wrangling.py
@@ -13,7 +13,25 @@ def analyze(dict):
     print("Analyzing...")
     for department in dict:
         nb_features = len(dict[str(department)].columns.values)
+        columns_with_nan = checkColumnsWithNan(dict[department])
+        nb_line = dict[department].all().sum()
         
-        
+        print("Department ", str(department), " have ", str(nb_line), " lines\n")
+        print("Departement ", str(department), " have NaN in : ", str(len(columns_with_nan)), "\n")
         print('Table "batiment" from department : ', str(department), " have ", str(nb_features), " different features\n")
-        
+
+def drop_nan(dict):
+    print("Removing NaN values... ")
+    for department in dict:
+        columns_with_nan = checkColumnsWithNan(dict[department])
+        for column in columns_with_nan:
+            dict[department].dropna(inplace=True)
+    return dict
+
+def checkColumnsWithNan(df):
+    columnsWithNaN = []
+    for column in df:
+        isNaN = (df[column].isna().sum())
+        if isNaN != 0:
+            columnsWithNaN.append([column, df[column].isna().sum()])
+    return columnsWithNaN

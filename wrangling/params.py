c9cf2f0d2c6f2b8991e9a596f8fa91b22dc2074c
Donovan <donovan.hoang@viacesi.fr>
2022-05-20 19:44:37 +0200

    Add comment on feature list (/wrangling/params.py)

--- wrangling/params.py
+++ wrangling/params.py
@@ -1,24 +1,21 @@
 features = [
-    'adedpe202006_logtype_baie_mat',
-    'adedpe202006_logtype_baie_remplissage',
-    'adedpe202006_logtype_baie_type_vitrage',
-    'adedpe202006_logtype_conso_ener',
-    # 'cerdvf2021v5_logtype_nb_sam',
-    # 'cerdvf2021v5_logtype_nb_chambre',
-    # 'cerdvf2021v5_logtype_nb_kitchenette',
-    # 'cerdvf2021v5_logtype_nb_cuisine',
-    # 'cerdvf2021v5_logtype_nb_agrem',
-    # 'cerdvf2021v5_logtype_nb_cave_cellier_remise',
-    # 'cerffo2020_nb_piece_annexe',
-    # 'cerffo2020_nb_piece_tx',
-    # 'cerffo2020_nb_sdb',
-    # 'cerffo2020_nb_wc',
-    'adedpe202006_logtype_mur_pos_isol_ext',
-    'adedpe202006_mean_conso_ener',
-    'adedpe202006_mean_estim_ges' ,
-    # 'mtedle2019_elec_conso_pro_par_m2_pro',
-    # 'mtedle2019_elec_conso_res_par_m2_res',
-    'mtedle2019_elec_conso_tot',
-    'mtedle2019_gaz_conso_pro', 
-    # 'mtedle2019_gaz_conso_pro_par_m2_pro_corr'
+    'bnb_id',                                       # identifiant unique
+    'commune',                                      # Nom de la commune
+    'adedpe202006_logtype_baie_mat',                # Matériaux de l'encadrement de la baie
+    'adedpe202006_logtype_baie_remplissage',        # Type de remplissage des vitrages argon ou air
+    'adedpe202006_logtype_baie_type_vitrage',       # Type de vitrage
+    'adedpe202006_logtype_conso_ener',              # Consommation d'énergie réglementaire (ancien DPE avant réforme 1er juillet 2021)
+    'adedpe202006_logtype_mur_pos_isol_ext',        # Type d'isolation des murs
+    'adedpe202006_mean_conso_ener',                 # Moyenne des consommations énergétiques sur l''ensemble des DPE [kWhEP/m².an]
+    'adedpe202006_mean_estim_ges' ,                 # Moyenne des émissions de GES sur l'ensemble des DPE [kgC02eq/m².an]
+    'mtedle2019_elec_conso_tot',                    # Consommation électrique totale [kWh/an]
+    'mtedle2019_gaz_conso_pro',                     # Consommation tertiaire gaz
+    'adedpe202006_logtype_ch_is_solaire',           # Présence d'énergie solaire pour le chauffage
+    'adedpe202006_logtype_ch_type_ener_corr',       # Type d'énergie de chauffage (concaténé + )
+    'adedpe202006_logtype_classe_conso_ener',       # Classe de consommation énergie d'un logement représentatif du bâtiment ayant un DPE
+    'adedpe202006_logtype_enr',                     # Energies renouvelables présentes dans le bâtiment
+    'adedpe202006_logtype_mur_pos_isol_ext',        # Type d'isolation des murs
+    'adedpe202006_logtype_perc_surf_vitree_ext'     # Pourcentage de surface vitrée d'un logement du bâtiment
+    'adedpe202006_logtype_presence_climatisation',  # Booléen présence de climatisation
+    'adedpe202006_logtype_s_hab'                    # Surface habitable du DPE
 ]
